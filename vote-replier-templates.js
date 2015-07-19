4f0b89b7efda9d5612a3de456bad72523daffbe7
brettimus <brbeut@gmail.com>
2015-07-19 16:03:30 -0700

    add a 'you are the first to vote' reply

--- vote-replier-templates.js
+++ vote-replier-templates.js
@@ -8,11 +8,60 @@ var updatedVote = "@{{voter}} "+
                     "You saved America (again)!\n\n"+
                     "I updated your vote for @{{candidate}} to {{vote}}.";
 
-var votedForSelf = "@{{voter}} You are America!!\n\n"+
+var votedForSelf = "@{{voter}}"+
+                    "You are America!!\n\n"+
                     "I'll remember next November that you are {{vote}}.";
 
-module.exports = {
+var firstVoteForCandidate = "@{{voter}} "+
+                "Hello, Patriot!\n\n"+
+                "You're the first to give @{{candidate}} "+
+                "a @{{vote}}.";
+
+templates = {
     createdVote: new Boo(createdVote),
     updatedVote: new Boo(updatedVote),
     votedForSelf: new Boo(votedForSelf),
+    firstVoteForCandidate: new Boot(firstVoteForCandidate),
 };
+
+module.exports.voteCreated = function(data) {
+    var tag = "\n\n\nOh, and Democracy says thanks.";
+    var result = templates.createdVote.compile(data);
+
+    if ((result + tag).length > 140) {
+        return result;
+    }
+    return (result + tag);
+};
+
+module.exports.voteUpdated = function(data) {
+    var tag = "\n\n\nDemocracy says thanks (again)";
+    var result = templates.updatedVote.compile(data);
+
+    if ((result + tag).length > 140) {
+        return result;
+    }
+    return (result + tag);
+};
+
+module.exports.firstVoteForCandidate = function(data) {
+    var tag = "\n\n\nRally your friends to do the same!";
+    var result = templates.firstVoteForCandidate.compile(data);
+
+    if ((result + tag).length > 140) {
+        return result;
+    }
+    return (result + tag);
+};
+
+module.exports.votedForSelf = function(data) {
+    var result = templates.votedForSelf.compile(data);
+    if (result.length > 140) {
+        result = trimResult(result);
+    }
+    return result;
+};
+
+function trimResult(string) {
+    return string.slice(0, 137)+"...";
+}
--- vote-replier.js
+++ vote-replier.js
@@ -21,47 +21,18 @@ function createReply(data) {
         voter: originalData.voter.handle,
         vote: originalData.vote,
     };
-    var reply;
 
-    if (templateData.voter === templateData.candidate) {
-        return votedForSelfMessage(templateData);
+    var votedForSelf = (templateData.voter === templateData.candidate);
+    if (data.isFirstVote) {
+        return templates.firstVoteForCandidate(templateData);
+    }
+    if (votedForSelf) {
+        return templates.votedForSelf(templateData);
     }
     else if (data.isNew) {
-        return voteCreatedMessage(templateData);
+        return templates.voteCreated(templateData);
     }
     else {
-        return voteUpdatedMessage(templateData);
+        return templates.voteUpdated(templateData);
     }
-}
-
-function voteCreatedMessage(data) {
-    var tag = "\n\n\nOh, and Democracy says thanks.";
-    var result = templates.createdVote.compile(data);
-
-    if ((result + tag).length > 140) {
-        result = trimResult(result);
-    }
-    return (result + tag);
-}
-
-function voteUpdatedMessage(data) {
-    var tag = "\n\n\nDemocracy says thanks (again)";
-    var result = templates.updatedVote.compile(data);
-
-    if ((result + tag).length > 140) {
-        return result;
-    }
-    return (result + tag);
-}
-
-function votedForSelfMessage(data) {
-    var result = templates.votedForSelf.compile(data);
-    if (result.length > 140) {
-        result = trimResult(result);
-    }
-    return result;
-}
-
-function trimResult(string) {
-    return string.slice(0, 137)+"...";
 }
