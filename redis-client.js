940b734843fd73a21f1ec45c636d0ab9bb22f890
brettimus <brbeut@gmail.com>
2015-07-26 16:43:15 -0700

    separate out redis config

--- redis-client.js
+++ redis-client.js
@@ -1,9 +1,11 @@
 var redis = require("redis");
-var port = process.env.REDIS_PORT;
-var host = process.env.REDIS_HOST;
+var config = require("./config/redis");
+
+var port = config.port;
+var host = config.host;
 
 module.exports = function() {
-    return redis.createClient(port, host, {});
+    return redis.createClient(port, host);
 };
 
 // NYI
