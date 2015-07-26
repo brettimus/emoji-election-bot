7e4b7d56b8c3b259fa5b8e05501d1591481926c1
brettimus <brbeut@gmail.com>
2015-07-26 02:05:21 -0700

    add instructions to the webapp first reply tweet

--- vote-replies/vote-first-app.js
+++ vote-replies/vote-first-app.js
@@ -4,7 +4,7 @@
 var Boo = require("boo-templates");
 
 var t = "@{{voter}} "+
-        "Thanks for voting, patriot!\n\n"+
+        "Thanks for voting, patriot! "+
         "Your {{vote}} for @{{candidate}} was cast.";
 
 var template = new Boo(t);
@@ -12,7 +12,7 @@ var template = new Boo(t);
 module.exports = voteFirstApp;
 
 function voteFirstApp(data) {
-    var tag = " Now try voting directly from Twitter!";
+    var tag = "\n\n Vote again on Twitter! https://medium.com/@emojielection/how-to-vote-in-the-emoji-election-58d108cefae5";
     var result = template.compile(data);
 
     if ((result + tag).length > 140) {
