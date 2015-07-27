1a5e76317068f9ffd9b10b27cb81d7d5581ae5f1
brettimus <brbeut@gmail.com>
2015-07-26 17:03:08 -0700

    add a test set command

--- index.js
+++ index.js
@@ -10,55 +10,63 @@ var credentials = require("./config/credentials");
 var client      = new Twitter(credentials);
 
 var r_cache = require("./redis-client")();
-console.log(r_cache);
-r_cache.on("ready", function(data) {
-    console.log("REDIS IS READY!!!", data);
+
+r_cache.on("ready", function() {
+    console.log("REDIS IS READY!!!");
+    test_redis();
+    emojiElection();
 });
 r_cache.on("error", function(err) {
-    console.log("GODDAMNIT REDIS!!!", err);
+    console.error("GODDAMNIT REDIS!!!", err);
 });
 
 var config     = require("./config");
 var BOT_HANDLE = config.BOT_HANDLE;
 
-client.stream('statuses/filter', { track: '@'+BOT_HANDLE }, function(stream) {
+function test_redis() {
+    r_cache.set("test", "true", redis.print);
+}
 
-    stream.on('data', function(tweet) {
-        var data = parseTweet(tweet);
-        if (data.voter.handle === BOT_HANDLE) {
-            console.log("[BOT]: Short circuited a false-vote by yours truly.");
-            return;
-        }
-        process.nextTick(function() {
-            validateVote(data, {
-                success: success,
-                failure: function(msg) { console.log(msg); },
+function emojiElection() {
+    client.stream('statuses/filter', { track: '@'+BOT_HANDLE }, function(stream) {
+
+        stream.on('data', function(tweet) {
+            var data = parseTweet(tweet);
+            if (data.voter.handle === BOT_HANDLE) {
+                console.log("[BOT]: Short circuited a false-vote by yours truly.");
+                return;
+            }
+            process.nextTick(function() {
+                validateVote(data, {
+                    success: success,
+                    failure: function(msg) { console.log(msg); },
+                });
             });
+
+            function success() {
+                sendVote(data, function(err, response, body) {
+                    if (err) {
+                        console.log("Error sending vote...", err);
+                        console.log("Time of error: ", (new Date()));
+                        return;
+                    }
+                    else {
+                        replyToVoter(
+                            client,
+                            body,
+                            replyCallback
+                        );
+                    }
+                });
+            }
         });
 
-        function success() {
-            sendVote(data, function(err, response, body) {
-                if (err) {
-                    console.log("Error sending vote...", err);
-                    console.log("Time of error: ", (new Date()));
-                    return;
-                }
-                else {
-                    replyToVoter(
-                        client,
-                        body,
-                        replyCallback
-                    );
-                }
-            });
-        }
+        stream.on('error', function(error) {
+            console.log(error);
+            throw error;
+        });
     });
-
-    stream.on('error', function(error) {
-        console.log(error);
-        throw error;
-    });
-});
+}
 
 // HACK 
 // This keeps shit alive on the server...
